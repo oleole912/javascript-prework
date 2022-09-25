e48c3c3aeb8f28a8239d6a998c97834516d98c4d
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-09-25 16:53:57 +0100

    Add logic for playerMove

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ if (randomNumber == '1') {
 }
 
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nozyce'; 
+} else {
+    printMessage('Bledna liczba. Podaj liczbe od 1 do 3');
+    playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
