dc98102a69210d44786d6d7b70a76af073933dab
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-09-25 16:46:00 +0100

    Add else if logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-var computerMove;
-computerMove = 'kamień';
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamien';
+} else if (computerMove == '2') {
+    computerMove = 'papier';
+} else if (computerMove == '3') {
+    computerMove = 'nozyce'
+}
 
-var playerMove;
-playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
