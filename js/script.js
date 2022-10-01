21fa0090cd7e966b0575643ba24c6d638b0d707f
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-10-01 18:22:14 +0100

    Code correction

--- js/script.js
+++ js/script.js
@@ -4,14 +4,10 @@ const buttonPaper = document.getElementById('button-paper');
 const buttonScissors = document.getElementById('button-scissors');
 
 function buttonClicked(argButtonName) {
-  let playerMove, computerMove, randomNumber;
   clearMessages();
-  console.log(argButtonName + ' został kliknięty');
-  playerMove = argButtonName;
-  
+
 
   function getMoveName(argMoveId) {
-    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
     if (argMoveId == 1) {
       return 'kamień';
     } else if (argMoveId == 2) {
@@ -27,12 +23,7 @@ function buttonClicked(argButtonName) {
 
   function displayResult(argPlayerMove, argComputerMove) {
 
-    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-    if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-      printMessage('Wygrywasz!');
-    } else if (argPlayerMove == 'kamień' && argComputerMove == 'nozyce') {
-      printMessage('Wygrywasz!');
-    } else if (argPlayerMove == 'nozyce' && argComputerMove == 'papier') {
+    if (argPlayerMove == 'papier' && argComputerMove == 'kamień' || argPlayerMove == 'kamień' && argComputerMove == 'nozyce' || argPlayerMove == 'nozyce' && argComputerMove == 'papier') {
       printMessage('Wygrywasz!');
     } else if (argPlayerMove == argComputerMove) {
       printMessage('Remis!');
@@ -41,13 +32,10 @@ function buttonClicked(argButtonName) {
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   }
-      
-  playerMove = argButtonName; 
-  console.log('ruch gracza to: ' + playerMove);
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
-  console.log('ruch komputera to: ' + computerMove);
+
+  const playerMove = argButtonName;
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  const computerMove = getMoveName(randomNumber);
   displayResult(playerMove, computerMove);
 }
 
